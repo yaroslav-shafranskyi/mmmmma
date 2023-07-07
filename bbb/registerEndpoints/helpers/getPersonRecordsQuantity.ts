2c100395bce59dc3f302d231abab854ba3c37b2b
sslavko <shafranskyiyaroslav@gmail.com>
2023-07-07 17:22:03 +0300

    fixed getPersonRecordsQuantity

--- bbb/registerEndpoints/helpers/getPersonRecordsQuantity.ts
+++ bbb/registerEndpoints/helpers/getPersonRecordsQuantity.ts
@@ -2,7 +2,7 @@ import { personsTbl } from "../../../constants";
 import { db } from "../../init";
 
 export const getPersonRecordsQuantity = async (id: number) => {
-  const data = await db(personsTbl).select("count").where({ id });
+  const data = await db(personsTbl).select("recordsQuantity").where({ id });
 
-  return data[0]?.count ?? 0;
+  return data[0]?.recordsQuantity ?? 0;
 };
